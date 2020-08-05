176b0646c7fe4c2feea199d724c100851c0731d7
Manuel Urias <mj.urias.r@gmail.com>
2020-08-05 14:47:01 -0600

    added subjects button

--- isep_courses_adapt/models/op_batch.py
+++ isep_courses_adapt/models/op_batch.py
@@ -30,3 +30,28 @@ class op_batch(models.Model):
     expiration_days = fields.Integer("Expiration days", default=0)
     date_diplomas = fields.Datetime("Date diplomas")
     modality_id = fields.Many2one('op.modalidad', string='Modality', related='course_id.modality_id')
+    subject_count = fields.Integer(compute='_compute_subject_count')
+
+    def _compute_subject_count(self):
+        """Compute the number of distinct subjects linked to the batch."""
+        for batch in self:
+            sub_count = len(batch.course_id.subject_ids)
+            batch.subject_count = sub_count
+
+    def action_open_subjects(self):
+        """Display the linked subscription and adapt the view to the number of records to display."""
+        self.ensure_one()
+        subjects = self.course_id.subject_ids
+        action = self.env.ref('openeducat_core.act_open_op_subject_view').read()[0]
+        if len(subjects) > 1:
+            action['domain'] = [('id', 'in', subjects.ids)]
+        elif len(subjects) == 1:
+            form_view = [(self.env.ref('openeducat_core.act_open_op_subject_view').id, 'form')]
+            if 'views' in action:
+                action['views'] = form_view + [(state, view) for state, view in action['views'] if view != 'form']
+            else:
+                action['views'] = form_view
+            action['res_id'] = subjects.ids[0]
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+        return action
