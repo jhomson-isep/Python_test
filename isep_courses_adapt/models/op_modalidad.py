9a7e0853b497efc2a07c90058897afc2309aea4e
Manuel Urias <mj.urias.r@gmail.com>
2020-08-08 10:52:33 -0600

    added import courses cron function

--- isep_courses_adapt/models/op_modalidad.py
+++ isep_courses_adapt/models/op_modalidad.py
@@ -8,4 +8,4 @@ class OpModalidad(models.Model):
     _description = "Modalidad"
 
     name = fields.Char('Name', size=128, required=True)
-    code = fields.Char('Name', size=8, required=True)
+    code = fields.Char('Code', size=8, required=True)
--- isep_courses_adapt/models/op_sql.py
+++ isep_courses_adapt/models/op_sql.py
@@ -37,8 +37,8 @@ class SQL():
         return rows
 
     def get_course_by_code(self, code):
-        rows = self.query(
-            "SELECT TOP (1) * FROM Cursos WHERE SUBSTRING(Curso_Id, 3,2)='{0}' ORDER BY id DESC;".format(code))
+        rows = self.query_get_one(
+            "SELECT * FROM Cursos WHERE SUBSTRING(Curso_Id, 3,2)='{0}' ORDER BY id DESC;".format(code))
         return rows
 
     def query(self, sql):
@@ -49,4 +49,14 @@ class SQL():
         cursor_sql = conn.cursor()
         cursor_sql.execute(sql)
         row = cursor_sql.fetchall()
+        return row
+
+    def query_get_one(self, sql):
+        # con_string = 'DSN=%s;UID=%s;PWD=%s;DATABASE=%s;' % (self.dsn, self.user, self.password, self.database)
+        con_string = 'DRIVER={SQL Server};SERVER=%s;UID=%s;PWD=%s;DATABASE=%s;' % (
+            self.server, self.user, self.password, self.database)
+        conn = pyodbc.connect(con_string)
+        cursor_sql = conn.cursor()
+        cursor_sql.execute(sql)
+        row = cursor_sql.fetchone()
         return row
