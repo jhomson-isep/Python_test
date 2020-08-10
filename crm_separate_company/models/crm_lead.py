6ecb225cc287f8beccd69dc79c0af4c750ffa6de
JoelMartinez97 <joelulisesmartinezfernandez@gmail.com>
2020-08-10 12:24:15 -0600

    add some logs and use write on create method

--- crm_separate_company/models/crm_lead.py
+++ crm_separate_company/models/crm_lead.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, SUPERUSER_ID
 from datetime import datetime
 import logging
 
@@ -13,10 +13,12 @@ class CrmLead(models.Model):
 
     @api.multi
     def create(self, lead):
+        res = super(CrmLead, self).create(lead)
+        self.sudo()
         #logger.info(lead)
         # Buscar el cliente mediante el email utilizando el self.env en el modelo res.partner
         client = self.env['res.partner'].search([('email', '=', lead.get('email_from'))], limit=1)
-        lead.update({"partner_id": client.id})
+        lead.update({'partner_id': client.id})
 
         # Buscar el id del país para poder vincular después
         #country_id = self.env['res.country'].search([('code', '=', lead.get('country_id'))])
@@ -36,52 +38,52 @@ class CrmLead(models.Model):
         # cod_curso = self.env['res.product.template'].search([('default_code', '=', lead.get(''))])
 
         # Hacer update a todos los datos antes de crear el lead
-        lead.update({"contact_name": lead.get('contact_name')})
-        lead.update({"phone": lead.get('phone')})
-        lead.update({"mobile": lead.get('mobile')})
+        lead.update({'contact_name': lead.get('contact_name')})
+        lead.update({'phone': lead.get('phone')})
+        lead.update({'mobile': lead.get('mobile')})
 
         #lead.update({"description": lead.get('description')})
-        lead.update({"x_titulacion": lead.get('x_titulacion')})
-        lead.update({"zip": lead.get('zip')})
+        lead.update({'x_titulacion': lead.get('x_titulacion')})
+        lead.update({'zip': lead.get('zip')})
 
-        lead.update({"country_id": lead.get('country_id')})
-        lead.update({"city": lead.get('city')})
-        lead.update({"x_annonacimiento": lead.get('x_annonacimiento')})
+        lead.update({'country_id': lead.get('country_id')})
+        lead.update({'city': lead.get('city')})
+        lead.update({'x_annonacimiento': lead.get('x_annonacimiento')})
 
-        lead.update({"email_from": lead.get('email_from')})
-        lead.update({"street": lead.get('street')})
+        lead.update({'email_from': lead.get('email_from')})
+        lead.update({'street': lead.get('street')})
         # lead.update({"company_id": lead.get('company_id')})
 
         #lead.update({"name": lead.get('name')})
-        lead.update({"create_date": lead.get('create_date')})
-        lead.update({"x_codsede": lead.get('x_codsede')})
+        lead.update({'create_date': lead.get('create_date')})
+        lead.update({'x_codsede': lead.get('x_codsede')})
 
         #lead.update({"x_codmodalidad": lead.get('x_codmodalidad')})
-        lead.update({"x_codarea": lead.get('x_codarea')})
-        lead.update({"x_finalizacionestudios": lead.get('x_finalizacionestudios')})
+        lead.update({'x_codarea': lead.get('x_codarea')})
+        lead.update({'x_finalizacionestudios': lead.get('x_finalizacionestudios')})
 
-        lead.update({"x_ga_campaign": lead.get('x_ga_campaign')})
-        lead.update({"x_content": lead.get('x_content')})
-        lead.update({"x_ga_medium": lead.get('x_ga_medium')})
+        lead.update({'x_ga_campaign': lead.get('x_ga_campaign')})
+        lead.update({'x_content': lead.get('x_content')})
+        lead.update({'x_ga_medium': lead.get('x_ga_medium')})
 
-        lead.update({"x_ga_utma": lead.get('x_ga_utma')})
-        lead.update({"x_ga_source": lead.get('x_ga_source')})
-        lead.update({"x_modalidad_id": lead.get('x_modalidad_id')})
+        lead.update({'x_ga_utma': lead.get('x_ga_utma')})
+        lead.update({'x_ga_source': lead.get('x_ga_source')})
+        lead.update({'x_modalidad_id': lead.get('x_modalidad_id')})
 
-        lead.update({"x_codtipodecurso": lead.get('x_codtipodecurso')})
-        lead.update({"user_id": user})
-        lead.update({"x_sede_id": lead.get('x_sede_id')})
+        lead.update({'x_codtipodecurso': lead.get('x_codtipodecurso')})
+        lead.update({'user_id': user})
+        lead.update({'x_sede_id': lead.get('x_sede_id')})
 
-        lead.update({"team_id": lead.get('team_id')})
+        lead.update({'team_id': lead.get('team_id')})
         #lead.update({"x_producto_id": 0})
         #lead.update({"x_area_id": 0})
 
         #lead.update({"x_curso_id": 0})
         #lead.update({"x_tipodecurso_id": ''})
-        lead.update({"x_grupoduplicado": ''})
+        lead.update({'x_grupoduplicado': ''})
 
-        lead.update({"x_numdups": 0})
-        lead.update({"x_precontactonuevodup": ''})
+        lead.update({'x_numdups': 0})
+        lead.update({'x_precontactonuevodup': ''})
         #lead.update({"type": "lead"})
         #lead.update({"company_id": lead.get('company_id')})
 
@@ -90,52 +92,65 @@ class CrmLead(models.Model):
         cod_curso = lead.get('x_codcurso')
         email = lead.get('email_from')
         modalidad = lead.get('x_codmodalidad')
+        cod_tipo_cuso = lead.get('x_codtipocurso')
 
         #Mediante url enviar a donde debe
         url = lead.get('x_ga_source')
         if url.find("ised") != -1:
             lead.update({'company_id': 4})
+            logger.info("Entre en ISED")
+
         elif url.find(".com") != -1:
             lead.update({'company_id': 1111})
+            logger.info("Entre en LATAM")
             pass;
         else:
             lead.update({'company_id': 1})
+            logger.info("Entre en España")
             pass;
 
         company_id = lead.get('company_id')
         #Problemas con el campo mal hecho de modalidad y sede, en los type form se llaman distinto por eso el cambio
-        if modalidad == "Presencial":
-            modalidad = "PRS"
-        elif modalidad == "Online":
-            modalidad = "ELR"
-
-        if cod_sede == 'centro-oviedo':
-            cod_sede = "OVI"
-        elif cod_sede == 'centro-bilbao':
-            cod_sede = "BIO"
-        elif cod_sede == 'centro-madrid-atocha':
-            cod_sede = "MAD"
-        elif cod_sede == 'centro-pamplona':
-            cod_sede = "PAM"
-        elif cod_sede == 'centro-zaragoza':
-            cod_sede = "ZAR"
+        if modalidad == 'Presencial':
+            modalidad = 'PRS'
+        elif modalidad == 'Online':
+            modalidad = 'ELR'
+            lead.update({'company_id': 3})
 
         #Actualizar la modalidad
         lead.update({'x_codmodalidad': modalidad})
+
+
+        if cod_sede == 'centro-oviedo':
+            cod_sede = 'OVI'
+        elif cod_sede == 'centro-bilbao':
+            cod_sede = 'BIO'
+        elif cod_sede == 'centro-madrid-atocha':
+            cod_sede = 'MAD'
+        elif cod_sede == 'centro-pamplona':
+            cod_sede = 'PAM'
+        elif cod_sede == 'centro-zaragoza':
+            cod_sede = 'ZAR'
+
+
 
         #REVISAR ESTO
         #Añadir producto a la iniciativa directamente
         logger.info(company_id)
         try:
             referencia_interna = self.env['product_template'].search([('default_code', '=', cod_curso)],limit=1)
-            lead.update({"x_curso_id": referencia_interna.id})
-            lead.update({"x_producto_id": referencia_interna.id})
+            lead.update({'x_curso_id': referencia_interna.id})
+            lead.update({'x_producto_id': referencia_interna.id})
         except:
-            print("No pudo relacionar la referencia interna con el cod_curso")
+            logger.info("No pudo relacionar la referencia interna con el cod_curso")
 
         #Crear nombre compuesto de su sede, el codigo y email
-        name = cod_sede + cod_curso + " - " + email
-        lead.update({"name": name})
+        if modalidad != 'Online':
+            name = cod_sede + cod_curso + " - " + email
+        else:
+            name = modalidad + cod_curso + " - " + email
+
+        lead.update({'name': name})
 
         #ISEP LATAM
         #---------------------------------
@@ -150,28 +165,33 @@ class CrmLead(models.Model):
         #ISED
         #---------------------------------
         elif company_id == 4:
-            if modalidad == "ONL":
+            if modalidad == 'ONL':
                 #Centro Sup de estudios ISED SL - Online
-                lead.update({"company_id": 3})
+                lead.update({'company_id': 3})
+                logger.info("Entre en Online")
 
-            elif cod_sede == "MAD":
+            elif cod_sede == 'MAD':
                 #Centro de estudios ISED SL - Madrid
-                lead.update({"company_id": 4})
+                lead.update({'company_id': 4})
+                logger.info("Entre en Madrid")
 
-            elif cod_sede == "BIO":
+            elif cod_sede == 'BIO':
                 #Centro de estudios ISED Bilbao - Bilbao
-                lead.update({"company_id": 5})
+                lead.update({'company_id': 5})
+                logger.info("Entre en Bilbao")
 
-            elif cod_sede == "ZAR":
+            elif cod_sede == 'ZAR':
                 #Zarised - Zaragoza
-                lead.update({"company_id": 6})
+                lead.update({'company_id': 6})
+                logger.info("Entre en Zaragoza")
 
             else:
                 #Iruñised - Pamplona
-                lead.update({"company_id": 22})
-
-
-
-        res = super(CrmLead, self).create(lead)
+                lead.update({'company_id': 22})
+                logger.info("Entre en Iruñised")
+        logger.info('\n Company ID \n')
+        logger.info(lead.get('company_id'))
+        res2 = super(CrmLead, self).write(lead)
+        logger.info(res2)
 
         return res
