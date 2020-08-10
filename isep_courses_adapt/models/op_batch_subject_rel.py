1853d3ee128dca72cb890c834198b977a10e4a59
Manuel Urias <mj.urias.r@gmail.com>
2020-08-10 11:17:16 -0600

    added translations for subjects and subjects rel

--- isep_courses_adapt/models/op_batch_subject_rel.py
+++ isep_courses_adapt/models/op_batch_subject_rel.py
@@ -4,13 +4,12 @@ from odoo import models, fields
 class OpBatchSubjectRel(models.Model):
     _name = 'op.batch.subject.rel'
 
-    batch_id = fields.Many2one('op.batch')
-    subject_id = fields.Many2one('op.subject')
-
+    batch_id = fields.Many2one('op.batch', string="Batch")
+    subject_id = fields.Many2one('op.subject', string="Subject")
     hours = fields.Float(string="Hours")
     credits = fields.Float(string="Credits")
     ects = fields.Integer(string="ECTS", default=0)
-    nif_faculty = fields.Char(string="Teacher's nif", size=20)
+    nif_faculty = fields.Char(string="Teacher's NIF", size=20)
     evaluable = fields.Boolean(string='Is evaluable', default=True)
     level = fields.Char(string="Level", size=1)
     optional = fields.Boolean(string='Is optional', default=False)
