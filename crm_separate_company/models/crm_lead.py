a9f91b081a96b314e3b4f41350e3e38a3d84a1ce
Joel <you@example.com>
2020-08-11 14:50:22 -0600

    erase unnecesary lead.uptdate and detail some lines

--- crm_separate_company/models/crm_lead.py
+++ crm_separate_company/models/crm_lead.py
@@ -24,75 +24,10 @@ class CrmLead(models.Model):
 
         res = super(CrmLead, self).create(lead)
 
-        # self.sudo(2)
-        #logger.info(lead)
-
-        # Buscar el id del país para poder vincular después
-        #country_id = self.env['res.country'].search([('code', '=', lead.get('country_id'))])
-        #lead.update({"country_id": country_id.id})
-
-        # Buscar el id del estado del país
-        #state_id = self.env['res.country.state'].search([('code', '=', lead.get('state_id'))])
-        #lead.update({"state_id": state_id.id})
-
-        # Buscar user id en res.partner
-        #user = client.user_id.id
-
-        # Tomar la fecha actual
-        date = datetime.now()
-
-        # Relación de codigo curso con referencia interna
-        # cod_curso = self.env['res.product.template'].search([('default_code', '=', lead.get(''))])
-
-        # Hacer update a todos los datos antes de crear el lead
-        lead.update({'contact_name': lead.get('contact_name')})
-        lead.update({'phone': lead.get('phone')})
-        lead.update({'mobile': lead.get('mobile')})
-
-        #lead.update({"description": lead.get('description')})
-        lead.update({'x_titulacion': lead.get('x_titulacion')})
-        lead.update({'zip': lead.get('zip')})
-
-        lead.update({'country_id': lead.get('country_id')})
-        lead.update({'city': lead.get('city')})
-        lead.update({'x_annonacimiento': lead.get('x_annonacimiento')})
-
-        lead.update({'email_from': lead.get('email_from')})
-        lead.update({'street': lead.get('street')})
-        # lead.update({"company_id": lead.get('company_id')})
-
-        #lead.update({"name": lead.get('name')})
-
-        lead.update({'x_codsede': lead.get('x_codsede')})
-
-        #lead.update({"x_codmodalidad": lead.get('x_codmodalidad')})
-        lead.update({'x_codarea': lead.get('x_codarea')})
-        lead.update({'x_finalizacionestudios': lead.get('x_finalizacionestudios')})
-
-        lead.update({'x_ga_campaign': lead.get('x_ga_campaign')})
-        lead.update({'x_content': lead.get('x_content')})
-        lead.update({'x_ga_medium': lead.get('x_ga_medium')})
-
-        lead.update({'x_ga_utma': lead.get('x_ga_utma')})
-        lead.update({'x_ga_source': lead.get('x_ga_source')})
-        lead.update({'x_modalidad_id': lead.get('x_modalidad_id')})
-
-        lead.update({'x_codtipodecurso': lead.get('x_codtipodecurso')})
-        #lead.update({'user_id': user})
-        lead.update({'x_sede_id': lead.get('x_sede_id')})
-
-        lead.update({'team_id': lead.get('team_id')})
-        #lead.update({"x_producto_id": 0})
-        #lead.update({"x_area_id": 0})
-
-        #lead.update({"x_curso_id": 0})
-        #lead.update({"x_tipodecurso_id": ''})
+        #Faltantes en el lead
         lead.update({'x_grupoduplicado': ''})
-
         lead.update({'x_numdups': 0})
         lead.update({'x_precontactonuevodup': ''})
-        #lead.update({"type": "lead"})
-        #lead.update({"company_id": lead.get('company_id')})
 
         #Lógica de las distintas empresas
         nombre = lead.get('name')
