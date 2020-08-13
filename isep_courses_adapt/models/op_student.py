7a8b6dd8c17cf03b3fb709cb53097559d90a1405
Manuel Urias <mj.urias.r@gmail.com>
2020-08-13 17:05:47 -0600

    added access rules for new models

--- isep_courses_adapt/models/op_student.py
+++ isep_courses_adapt/models/op_student.py
@@ -8,12 +8,11 @@ class OpStudent(models.Model):
     _name = 'op.student'
     _inherit = ['op.student', 'mail.thread']
 
-    partner_id = fields.Many2one('res.partner', string='Partner')
     campus_id = fields.Many2one('op.campus', string='Campus')
     uvic_documentation = fields.Boolean(string='UVIC Documentation', default=False)
     rvoe_documentation = fields.Boolean(string='RVOE Documentation', default=False)
     curp = fields.Char(string='CURP', size=20)
-    yer_end_studies = fields.Integer(string='Year of completion of studies')
+    year_end_studies = fields.Integer(string='Year of completion of studies')
     document_type_id = fields.Many2one('op.document.type', string='Document type')
     document_number = fields.Char(string='Document number', size=32)
     contact_type_id = fields.Many2one('op.contact.type', string='Contact type')
