5e2e092f69563520c9815ac51a19035bc76fdb2c
JoelMartinez97 <joelulisesmartinezfernandez@gmail.com>
2020-08-17 09:16:08 -0600

    add module of minimal changes

---
+++ isep_minimal_changes/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "ISEP cambios minimos",
+    "summary": """Cambios minimos en vistas o módulos""",
+    "description": """
+        Cambios pequeños en algunas vistas o módulos
+    """,
+    "version": "12.0.1.0.0",
+    "author": "Isep Latam, SC",
+    "website": "https://www.isep.es/contacto/",
+    "category": "Education",
+    "license": "AGPL-3",
+    "depends": [
+        "base"
+    ],
+    "data": [
+        "security/ir.model.access.csv"
+    ],
+    'installable': True,
+}
