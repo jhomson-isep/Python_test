5fdc0efee62763e72686e7899aed8ac1f82d243e
Manuel Urias <mj.urias.r@gmail.com>
2020-09-12 13:28:29 -0600

    custom_contact: refactor module

--- custom_contact/models/res_partner.py
+++ custom_contact/models/res_partner.py
@@ -5,178 +5,63 @@ from odoo.exceptions import UserError
 import re
 
 
-# class extra-addons/custom_contact(models.Model):
-#     _name = 'extra-addons/custom_contact.extra-addons/custom_contact'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
-
-class _Customfield(models.Model):
+class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     @api.model
     def create(self, values):
-        correo = values.get('email')
-        if correo:
-            if re.match('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,15}$', str(correo).lower()):
-                direccion, dominio = correo.split("@")
-                if dominio == "gmali.com":
-                    dominio = "gmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "gmial.com":
-                    dominio = "gmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "gmail.cm":
-                    dominio = "gmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "gmal.com":
-                    dominio = "gmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "gmil.com":
-                    dominio = "gmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "hotmal.com":
-                    dominio = "hotmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "hotmail.com.mx":
-                    dominio = "hotmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "hotmai.com":
-                    dominio = "hotmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "hotmial.com":
-                    dominio = "hotmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "jotmail.com":
-                    dominio = "hotmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "hotmali.com":
-                    dominio = "hotmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "hotmail.cm":
-                    dominio = "hotmail.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "hotmal.es":
-                    dominio = "hotmail.es"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "hotmial.es":
-                    dominio = "hotmail.es"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "jotmail.es":
-                    dominio = "hotmail.es"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "hotmali.es":
-                    dominio = "hotmail.es"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "outlok.es":
-                    dominio = "outlook.es"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "outlock.es":
-                    dominio = "outlook.es"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "outloc.es":
-                    dominio = "outlook.es"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "oulook.es":
-                    dominio = "outlook.es"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "outlok.com":
-                    dominio = "outlook.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "outlock.com":
-                    dominio = "outlook.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "outloc.com":
-                    dominio = "outlook.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "oulook.com":
-                    dominio = "outlook.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "outlook.cm":
-                    dominio = "outlook.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "yaho.com":
-                    dominio = "yahoo.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "yajoo.com":
-                    dominio = "yahoo.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "jahoo.com":
-                    dominio = "yahoo.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "yahoo.cm":
-                    dominio = "yahoo.com"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "yaho.es":
-                    dominio = "yahoo.es"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "yajoo.es":
-                    dominio = "yahoo.es"
-                    correo = ""
-                    correo = direccion
-                elif dominio == "jahoo.es":
-                    dominio = "yahoo.es"
-                    correo = ""
-                    correo = direccion
-                correo = direccion + '@' + dominio
-                values.update({'email': correo})
-                res = super(_Customfield, self).create(values)
-                return res
-            else:
-                raise UserError("Formato de correo no válido")
-        elif not correo:
+        email = values.get('email')
+        if email:
+            email = self.check_email(email)
+            values.update({'email': email})
+        else:
             raise UserError('Introduzca una dirección de correo electrónico')
+
+        res = super(ResPartner, self).create(values)
+        return res
 
     @api.multi
     def write(self, values):
-        res = super(_Customfield, self).write(values)
+        email = values.get('email')
+        if email:
+            email = self.check_email(email)
+            values.update({'email': email})
+        res = super(ResPartner, self).write(values)
         return res
 
-    """@staticmethod
-    def es_correo_valido(correo):
-        if re.match('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,15}$', str(correo).lower()):
-            direccion, dominio = correo.split("@")
-            if dominio == "gmali.com":
-                dominio = "gmail.com"
-                correo = ""
-                correo = direccion
-            correo = direccion + '@' + dominio
-            return correo
+    @staticmethod
+    def check_email(email):
+        gmail = ['gmali.com', 'gmial.com', 'gmail.cm', 'gmal.com', 'gmil.com']
+        hotmail = ['hotmal.com', 'hotmail.com.mx', 'hotmai.com',
+                   'hotmial.com', 'jotmail.com', 'hotmali.com', 'hotmail.cm']
+        hotmail_es = ['hotmal.es', 'hotmai.es', 'hotmial.es',
+                      'jotmail.es', 'hotmali.es', 'hotmail.es']
+        outlook = ['outlok.com', 'outlock.com', 'outloc.com', 'oulook.com',
+                   'oulook.com', 'otlook.com', 'outluk.com']
+        outlook_es = ['outlok.es', 'outlock.es', 'outloc.es', 'oulook.es',
+                      'oulook.es', 'otlook.es', 'outluk.es']
+        yahoo = ['yaho.com', 'yajoo.com', 'jahoo.com', 'yahoo.cm']
+        yahoo_es = ['yaho.es', 'yajoo.es', 'jahoo.es']
+        if re.match('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,15}$',
+                    str(email).lower()):
+            address, domain = email.split("@")
+
+            if domain in gmail:
+                domain = 'gmail.com'
+            if domain in hotmail:
+                domain = 'hotmail.com'
+            if domain in hotmail_es:
+                domain = 'hotmail.es'
+            if domain in outlook:
+                domain = 'outlook.com'
+            if domain in outlook_es:
+                domain = 'outlook.es'
+            if domain in yahoo:
+                domain = 'yahoo.com'
+            if domain in yahoo_es:
+                domain = 'yahoo.es'
         else:
-            raise UserError("Dirección de correo en formato no válido")"""
+            raise UserError("Invalid format code")
+
+        email = address + '@' + domain
+        return email
