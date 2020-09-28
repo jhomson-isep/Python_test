9a404cf50cba906168b533a73fb9f55ba09b2e2f
JoelMartinez97 <joelulisesmartinezfernandez@gmail.com>
2020-09-28 12:39:32 -0600

    add new field active to sale order and new module of reports

---
+++ isep_reports_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Reportes para ISEP',
+    'summary': """Reportes para ISEP""",
+    'description': """
+        Modulo para los reportes, matriculas y facturas (Solo vistas).
+    """,
+    'version': '12.0.1.0.0',
+    'author': 'Isep Latam, SC',
+    'website': 'https://www.isep.es/contacto/',
+    'category': 'Education',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'sale'
+    ],
+    'installable': True,
+}
