f1237f56b1c6abc70b4aadfb737876fdd619603e
Manuel Urias <mj.urias.r@gmail.com>
2020-11-06 08:18:40 -0700

    fixed orders list on portal

--- isep_portal_attachments/controllers/portal.py
+++ isep_portal_attachments/controllers/portal.py
@@ -2,35 +2,61 @@ from datetime import date
 from odoo import fields, http, _
 from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
-import base64
 from collections import OrderedDict
 from odoo.addons.portal.controllers.mail import _message_post_helper
-from odoo.tools import image_resize_image
-from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
-from odoo.addons.portal.controllers.portal import pager as portal_pager, \
-    get_records_pager
-from odoo.addons.purchase.controllers.portal import CustomerPortal as PurchasePortal
-from odoo.addons.web.controllers.main import Binary
-from odoo.osv import expression
+from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.addons.portal.controllers.portal import pager as portal_pager
 import logging
 
 logger = logging.getLogger(__name__)
 
-class PurchasePortalInherit(PurchasePortal):
+
+class CustomerPurchasePortal(CustomerPortal):
+
+    def _prepare_portal_layout_values(self):
+        values = super(CustomerPurchasePortal,
+                       self)._prepare_portal_layout_values()
+        values['purchase_count'] = request.env['purchase.order'].search_count([
+            ('state', 'in', ['sent', 'purchase', 'done', 'cancel'])
+        ])
+        values['searchbar_filters'] = {
+            'all': {'label': _('All'), 'domain': [
+                ('state', 'in', ['sent', 'purchase', 'done', 'cancel'])]},
+            'purchase': {'label': _('Purchase Order'),
+                         'domain': [('state', '=', 'purchase')]},
+            'cancel': {'label': _('Cancelled'),
+                       'domain': [('state', '=', 'cancel')]},
+            'done': {'label': _('Locked'), 'domain': [('state', '=', 'done')]},
+        }
+        logger.info("values: {0}".format(values))
+        return values
+
     @http.route(['/my/purchase', '/my/purchase/page/<int:page>'], type='http',
                 auth="user", website=True)
     def portal_my_purchase_orders(self, page=1, date_begin=None, date_end=None,
                                   sortby=None, filterby=None, **kw):
-        res = super(PurchasePortalInherit, self).portal_my_purchase_orders(
-            page=1, date_begin=None, date_end=None,
-            sortby=None, filterby=None, **kw)
         values = self._prepare_portal_layout_values()
+        partner = request.env.user.partner_id
+        PurchaseOrder = request.env['purchase.order']
 
         domain = []
 
+        archive_groups = self._get_archive_groups('purchase.order', domain)
         if date_begin and date_end:
             domain += [('create_date', '>', date_begin),
                        ('create_date', '<=', date_end)]
+
+        searchbar_sortings = {
+            'date': {'label': _('Newest'),
+                     'order': 'create_date desc, id desc'},
+            'name': {'label': _('Name'), 'order': 'name asc, id asc'},
+            'amount_total': {'label': _('Total'),
+                             'order': 'amount_total desc, id desc'},
+        }
+        # default sort by value
+        if not sortby:
+            sortby = 'date'
+        order = searchbar_sortings[sortby]['order']
 
         searchbar_filters = {
             'all': {'label': _('All'), 'domain': [
@@ -41,28 +67,44 @@ class PurchasePortalInherit(PurchasePortal):
                        'domain': [('state', '=', 'cancel')]},
             'done': {'label': _('Locked'), 'domain': [('state', '=', 'done')]},
         }
+        # default filter by value
         if not filterby:
             filterby = 'all'
         domain += searchbar_filters[filterby]['domain']
 
+        # count for pager
+        purchase_count = PurchaseOrder.search_count(domain)
+        # make pager
+        pager = portal_pager(
+            url="/my/purchase",
+            url_args={'date_begin': date_begin, 'date_end': date_end},
+            total=purchase_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # search the purchase orders to display, according to the pager data
+        orders = PurchaseOrder.search(
+            domain,
+            order=order,
+            limit=self._items_per_page,
+            offset=pager['offset']
+        )
+        request.session['my_purchases_history'] = orders.ids[:100]
+
         values.update({
             'date': date_begin,
+            'orders': orders,
+            'page_name': 'purchase',
+            'pager': pager,
+            'archive_groups': archive_groups,
+            'searchbar_sortings': searchbar_sortings,
             'sortby': sortby,
             'searchbar_filters': OrderedDict(
                 sorted(searchbar_filters.items())),
             'filterby': filterby,
             'default_url': '/my/purchase',
         })
-        return res
-
-class CustomerPurchasePortal(CustomerPortal):
-
-    def _prepare_portal_layout_values(self):
-        values = super(CustomerPurchasePortal, self)._prepare_portal_layout_values()
-        values['purchase_count'] = request.env['purchase.order'].search_count([
-            ('state', 'in', ['sent', 'purchase', 'done', 'cancel'])
-        ])
-        return values
+        return request.render("purchase.portal_my_purchase_orders", values)
 
     @http.route(['/my/purchase/<int:order_id>/accept'], type='json',
                 auth="public", website=True)
