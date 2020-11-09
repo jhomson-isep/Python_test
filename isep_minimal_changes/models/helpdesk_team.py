9345ba850cc8cb831a9c67372a9c436a38cd3607
JoelMartinez97 <joelulisesmartinezfernandez@gmail.com>
2020-11-09 11:19:48 -0700

    add field ticket_notify on helpdesk.team

---
+++ isep_minimal_changes/models/helpdesk_team.py
@@ -0,0 +1,7 @@
+from odoo import models, fields
+
+
+class HelpDeskTeam(models.Model):
+    _inherit = 'helpdesk.team'
+    ticket_notify = fields.Many2many('res.partner',
+                                     string="Notificar a:")
