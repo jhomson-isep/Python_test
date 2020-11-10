e751e24f2359bef092d0e467150457da5ed2382c
Manuel Urias <mj.urias.r@gmail.com>
2020-11-09 17:09:08 -0700

    added func to get email

--- isep_minimal_changes/models/helpdesk_team.py
+++ isep_minimal_changes/models/helpdesk_team.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class HelpDeskTeam(models.Model):
     _inherit = 'helpdesk.team'
     ticket_notify = fields.Many2many('res.partner',
                                      string="Notificar a:")
+
+    @staticmethod
+    def get_partner_emails(partner_ids):
+        emails = []
+        for partner in partner_ids:
+            if partner.email:
+                emails.append(partner.email)
+        return ', '.join(emails)
