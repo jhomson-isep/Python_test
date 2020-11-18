1bc94b351047730bb73e60238b4f231393af89e7
murias <murias@isep.com>
2020-11-18 16:27:24 -0700

    [Add] invoice filter on purchase order

--- isep_portal_attachments/models/purchase_order.py
+++ isep_portal_attachments/models/purchase_order.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from odoo.tools.float_utils import float_compare
-from odoo import models, fields
+from odoo import models, fields, api
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class PurchaseOrder(models.Model):
@@ -20,6 +23,10 @@ class PurchaseOrder(models.Model):
     signed_by = fields.Char('Signed by',
                             help='Name of the person that signed the SO.',
                             copy=False)
+    has_invoice_attached = fields.Boolean('Has attachments', default=False,
+                                          compute='_set_has_invoice_attached',
+                                          search='_attached_search',
+                                          stored=True)
 
     def has_to_be_signed(self, also_in_draft=False):
         return (self.state == 'sent' or (
@@ -28,7 +35,24 @@ class PurchaseOrder(models.Model):
     def has_attachments(self):
         attachments = self.env['ir.attachment'].search_count([(
             'res_model', '=', self._name), ('res_id', '=', self.id)])
+        if attachments > 0:
+            self.has_invoice_attached = True
         return attachments
+
+    def _set_has_invoice_attached(self):
+        for order in self:
+            attachments = self.env['ir.attachment'].search_count([(
+                'res_model', '=', order._name), ('res_id', '=', order.id)])
+            logger.debug(attachments)
+            if attachments > 0:
+                order.has_invoice_attached = True
+
+    @api.multi
+    def _attached_search(self, operator, value):
+        recs = self.search([]).filtered(
+            lambda x: x.has_invoice_attached is True)
+        if recs:
+            return [('id', 'in', [x.id for x in recs])]
 
     def action_create_purchase_invoice(self):
         invoice = self.env['account.invoice'].create({
