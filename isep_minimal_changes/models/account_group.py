37598bc62817ab7fefe97981f49dba0fb99f5f91
JoelMartinez97 <joelulisesmartinezfernandez@gmail.com>
2020-11-18 11:19:05 -0700

    delete account_group

--- isep_minimal_changes/models/account_group.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class AccountGroup(models.Model):
-    _inherit = 'account.group'
-
-    active = fields.Boolean(string='active', default=True)
