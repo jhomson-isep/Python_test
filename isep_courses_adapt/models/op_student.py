4e25c99432075dbd5b1d61a7ba888ec0209ea2f7
iameduard <iameduard@hotmail.com>
2020-11-20 18:08:26 -0400

    restore _get_last_access in op_student.py

--- isep_courses_adapt/models/op_student.py
+++ isep_courses_adapt/models/op_student.py
@@ -48,6 +48,23 @@ class OpStudent(models.Model):
         'N_ID Number must be unique per student!'
     )]
 
+    def _get_last_access(self):
+        for record in self:
+            access_ago = fields.Datetime.now() - record.student_access
+            minutes, seconds = divmod(access_ago.seconds, 60)
+            hours, minutes = divmod(minutes, 60)
+            access_string = ""
+            if access_ago.days > 0:
+                if access_ago.days > 365:
+                    years, days = divmod(access_ago.days, 365)
+                    access_string += "{0} años, {1} días, ".format(years, days)
+                else:
+                    access_string += "{0} días, ".format(access_ago.days)
+            if hours > 0:
+                access_string += "{0} horas, ".format(hours)
+            if minutes > 0:
+                access_string += "{0} minutos, ".format(minutes)
+            record.last_access = access_string[:-2]
 
     def equal_datetimes_YYMMDDHHmm(self,ddtime1,ddtime2):
         if isinstance(ddtime1, datetime.datetime) and\
