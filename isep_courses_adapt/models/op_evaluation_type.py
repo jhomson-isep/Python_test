63e40feea899b27ce4fc55deaf32b05e45a70e73
rlosm <rmileno@isep.com>
2020-11-30 17:45:23 -0400

    add table op_section_course Task:Improvement Proposals #1

--- isep_courses_adapt/models/op_evaluation_type.py
+++ isep_courses_adapt/models/op_evaluation_type.py
@@ -7,4 +7,5 @@ class OpEvaluationType(models.Model):
     _name = "op.evaluation.type"
     _description = "Course evaluation type"
 
-    name = fields.Char('Name', size=128, required=True)
+    name = fields.Char('Section Name', size=128, required=True)
+
