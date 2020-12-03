cfc94e53029a2c36fdc8519b18272e76bf322a1c
JoelMartinez97 <joelulisesmartinezfernandez@gmail.com>
2020-12-03 13:30:50 -0700

    add Uvic Report

--- isep_openeducat_reports/__manifest__.py
+++ isep_openeducat_reports/__manifest__.py
@@ -2,10 +2,11 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Reportes para ISEP',
-    'summary': """Reportes para ISEP""",
+    'name': 'Reportes OpenEducat ISEP',
+    'summary': """Reportes OpenEducat ISEP""",
     'description': """
-        Modulo para los reportes, matriculas y facturas (Solo vistas).
+        Modulo para los reportes:
+        - UVIC
     """,
     'version': '12.0.1.0.0',
     'author': 'Isep Latam, SC',
@@ -14,13 +15,10 @@
     'license': 'AGPL-3',
     'depends': [
         'base',
-        'sale'
+        'openeducat_core'
     ],
     'installable': True,
     'data': [
-        'reports/factura_isep_espana.xml',
-        'reports/hoja_matricula_ISEPSL.xml',
-        'reports/hoja_matricula_ISED.xml'
-
+        'reports/report_uvic_students.xml'
     ]
 }
