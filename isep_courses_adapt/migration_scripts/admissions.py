a13d19cd5d1119312539cfe4bc5071dc76aa1177
yorlysoro <yorlysoro@gmail.com>
2020-12-30 18:16:58 -0400

    Fixed some errors not-null violation constrain

--- isep_courses_adapt/migration_scripts/admissions.py
+++ isep_courses_adapt/migration_scripts/admissions.py
@@ -6,7 +6,8 @@ from sqlachemy_conn import *
 # SQL SERVER SESSION
 session_server = get_session_server_isep()
 # SQL SERVER AREAS
-matriculaciones = session_server.query(Matriculaciones).all()
+matriculaciones = session_server.query(Matriculaciones).\
+                                filter(Matriculaciones.FechaMatricula != None)
 # POSTGRES SERVER SESSION
 session_pg = get_pg_session()
 
@@ -71,11 +72,11 @@ for matricula in matriculaciones:
                 admission.state_id = partner.state_id
                 admission.register_id = admission_register.id
                 admission.application_number = application_number
-                admission.application_date = matricula.FechaMatricula
-                admission.admission_date = matricula.FechaMatricula
+                admission.application_date = matricula.FechaMatricula.date()
+                admission.admission_date = matricula.FechaMatricula.date()
                 admission.course_id = admission_register.course_id
                 admission.batch_id = batch.id
-                admission.unsubscribed_date = unsubscribed_date
+                admission.unsubscribed_date = unsubscribed_date.date()
                 admission.academic_record_closing = academic_record_closing
                 admission.exam_on_campus = exam_on_campus
                 admission.due_date = matricula.FechaVencimiento
