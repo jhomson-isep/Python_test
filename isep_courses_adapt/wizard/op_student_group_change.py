12977cefc5a6d3790f33126a8f8f39c92f0cbf66
rlosm <rmileno@isep.com>
2021-01-18 16:17:29 -0400

    Adding group and/or course change functionality

--- isep_courses_adapt/wizard/op_student_group_change.py
+++ isep_courses_adapt/wizard/op_student_group_change.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 class OpStudentGroupChangeWizard(models.TransientModel):
     _name = 'op.student.group.change.wizard'
@@ -19,18 +20,58 @@ class OpStudentGroupChangeWizard(models.TransientModel):
     def setDomainAdmission(self):
         return {'domain': {'op_admission_id': [('student_id', '=', self.getIdStudent())]}}
 
-    @api.onchange('op_course_id')
+    @api.onchange('op_course_id', 'op_modality_id')
     def setDomainBatch(self):
         return {'domain': {
             'op_batch_id': [('code', 'ilike', self.op_course_id.code),
                             ('code', 'ilike', self.op_modality_id.code)]}}
 
+    def modifiedDataInModels(self):
+        op_student_course = self.env['op.student.course'].search(
+            [('student_id', '=', self.getIdStudent()),
+             ('course_id', '=', self.op_admission_id.course_id.id)])
+        op_exam_attendees = self.env['op.exam.attendees'].search(
+            [('student_id', '=', self.getIdStudent()),
+             ('course_id', '=', self.op_admission_id.course_id.id)])
+        op_admission = self.env['op.admission'].search(
+            [('id', '=', self.op_admission_id.id)])
+        print(op_student_course)
+        print(op_exam_attendees)
 
+        if len(op_student_course) > 0:
+            values = {
+                'batch_id': self.op_batch_id.id,
+                'course_id': self.op_course_id.id
+            }
+            op_student_course.write(values)
 
+        if len(op_exam_attendees) > 0:
+            for op_exam_attendee in op_exam_attendees:
+                values = {
+                    'batch_id': self.op_batch_id.id,
+                    'course_id': self.op_course_id.id
+                }
+                op_exam_attendee.write(values)
 
+        if len(op_admission) > 0:
+            values = {
+                'batch_id': self.op_batch_id.id,
+                'course_id': self.op_course_id.id
+            }
+            op_admission.write(values)
 
-
-
-
-
-
+    def change(self):
+        if not self.op_admission_id.id:
+            raise ValidationError(
+                _("You must select admission."))
+        if not self.op_modality_id.id:
+            raise ValidationError(
+                _("You must select modality."))
+        if not self.op_course_id.id:
+            raise ValidationError(
+                _("You must select course."))
+        if not self.op_batch_id.id:
+            raise ValidationError(
+                _("You must select group."))
+        else:
+            self.modifiedDataInModels()
