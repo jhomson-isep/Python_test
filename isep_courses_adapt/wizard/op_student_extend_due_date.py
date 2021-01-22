06e0948f1a338c75e611cb832fb765f4fb96a996
yorlysoro <yorlysoro@gmail.com>
2021-01-22 09:22:50 -0400

    ADD message post and send email extend due date && ADD translation

--- isep_courses_adapt/wizard/op_student_extend_due_date.py
+++ isep_courses_adapt/wizard/op_student_extend_due_date.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
-import datetime
+from odoo.tools import date_utils
+import logging
+
+logger = logging.getLogger(__name__)
 
 class OpStudentExtendDueDate(models.TransientModel):
     _name = "op.student.extend.due.date.wizard"
@@ -30,11 +33,48 @@ class OpStudentExtendDueDate(models.TransientModel):
     def extend(self):
         for admission in self.admission_ids:
             new_date = self.calculate_date(admission.due_date)
-            print(new_date)
+            admission.update({
+                'due_date' : new_date,
+                })
+            logger.info("Update due date to admission %s" % admission.application_number)
+        body = '''<p> Se ha realizado una prórroga de %s %s a los siguientes cursos:</p>
+        ''' % (self.months_to_extend, 'mes' if self.months_to_extend == 1 else 'meses')
+        for admission in self.admission_ids:
+            body = body + '''<p> <strong>Admisión:</strong> %s </p>
+            <p><strong>Curso:</strong> %s </p> 
+            <p><strong>Grupo:</strong> %s </p>
+            <p><strong>Fecha de Vencimiento:</strong> %s </p>
+            ___________________________________
+            ''' %(admission.application_number, admission.course_id.name,
+                    admission.batch_id.code, admission.due_date)
+        for admission in self.admission_ids:
+            message = admission.student_id.message_post(body=body, subject="Prórroga" ,message_type='comment')
+            admission.student_id.env['mail.mail'].create({ 'mail_message_id' : message.id,
+                                                            'body_html' : body,
+                                                            'notification' : True,
+                                                            'recipient_ids' : [(4, admission.partner_id.id)],
+                                                            'auto_delete' : True,
+                                                            'references' : message.message_id,
+                                                            'headers' : "'{'X-Odoo-Objects': 'op.student-1'}'"})
+            mail_id = self.env['mail.mail'].search([])
+            admission.student_id.env['mail.notification'].create({ 
+                                                            'mail_message_id' : message.id,
+                                                            'res_partner_id'  : admission.partner_id.id,
+                                                            'is_read' : True,
+                                                            'is_email' : True,
+                                                            'mail_id' : mail_id[0].id   
+                                                             })
+            message.write({
+                            "subtype_id": 1,
+                            'needaction_partner_ids': [(4,admission.partner_id.id)],
+                            'partner_ids' : [(4,admission.partner_id.id)],
+                            })
+            break
+        # template = self.env['mail.template'].search(
+        #     [('name', '=', 'Email Extend Due Date Message')])
+        # template.send_mail(self.id, force_send=True,
+        #                     raise_exception=True)
 
-    def calculate_date(self, due_date):
-        due_date = datetime.datetime(due_date.year, due_date.month, due_date.day)
-        due_seconds = datetime.datetime.timestamp(due_date)
-        months_seconds = self.months_to_extend * 2.628e+6
-        new_date = due_seconds + months_seconds
-        return datetime.datetime.fromtimestamp(new_date).date()
+    def calculate_date(self, date):
+        new_date = date_utils.add(date, months=self.months_to_extend)
+        return new_date
