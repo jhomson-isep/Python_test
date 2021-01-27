11eda3b4d808208642e26bc5b8eb805cfa7f8e06
yorlysoro <yorlysoro@gmail.com>
2021-01-27 14:59:30 -0400

    Added history log message in change of group and change group in moodle

--- isep_courses_adapt/migration_scripts/subjects.py
+++ isep_courses_adapt/migration_scripts/subjects.py
@@ -11,21 +11,24 @@ logger = logging.getLogger(__name__)
 
 logger.warning("Start up script subjects import")
 for subject in subjects:
-    op_subject = session_pg.query(OpSubject).\
-                filter(OpSubject.code == subject.CodAsignatura).first()
-    if op_subject is None:
-        op_subject = OpSubject()
-        op_subject.name = subject.NomAsignatura
-        op_subject.code = subject.CodAsignatura
-        op_subject.type = 'theory'
-        op_subject.subject_type = 'compulsory'
-        op_subject.uvic_code = subject.CodUvic
-        op_subject.moodle_course_id = subject.Moodle
-        op_subject.active = True
-        session_pg.add(op_subject)
-        session_pg.commit()
-        logger.warning("Subject register code: %s" %  subject.CodAsignatura)
-    else:
-        logger.warning("Subject Already exist code: %s" %  subject.CodAsignatura)
+    try:
+        op_subject = session_pg.query(OpSubject).\
+                    filter(OpSubject.code == subject.CodAsignatura).first()
+        if op_subject is None:
+            op_subject = OpSubject()
+            op_subject.name = subject.NomAsignatura
+            op_subject.code = subject.CodAsignatura
+            op_subject.type = 'theory'
+            op_subject.subject_type = 'compulsory'
+            op_subject.uvic_code = subject.CodUvic
+            op_subject.moodle_course_id = subject.Moodle
+            op_subject.active = True
+            session_pg.add(op_subject)
+            session_pg.commit()
+            logger.warning("Subject register code: %s" %  subject.CodAsignatura)
+        else:
+            logger.warning("Subject Already exist code: %s" %  subject.CodAsignatura)
+    except Exception as e:
+        logger.info(e)
 
 logger.warning("End of script subjects import")
