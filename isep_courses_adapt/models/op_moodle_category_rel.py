3089cb69a3d4f950d30dba8b9a1c85474900a35c
yorlysoro <yorlysoro@gmail.com>
2021-02-09 12:41:12 -0400

    Added model moodle category rel

---
+++ isep_courses_adapt/models/op_moodle_category_rel.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+class op_moodle_category_rel(models.Model):
+    _name = 'op.moodle.category.rel'
+    _description = "Moodle Relation Course"
+    
+    name = fields.Char()
+    moodle_category = fields.Integer()
+    moodle_module = fields.Integer()
+    modality_id = fields.Many2many('op.modality')
+    course_id = fields.Many2one('op.course')
+    
