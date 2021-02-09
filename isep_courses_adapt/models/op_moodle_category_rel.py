c3157f24d6ab78972b0d05fa1b0c482520322acd
yorlysoro <yorlysoro@gmail.com>
2021-02-09 19:08:03 -0400

    merge commit

--- isep_courses_adapt/models/op_moodle_category_rel.py
+++ isep_courses_adapt/models/op_moodle_category_rel.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields
 
-class op_moodle_category_rel(models.Model):
+class OpMoodleCategoryRel(models.Model):
     _name = 'op.moodle.category.rel'
     _description = "Moodle Relation Course"
     
-    name = fields.Char()
-    moodle_category = fields.Integer()
+    name = fields.Char(string="Name")
+    code = fields.Char(string="Code")
+    moodle_category = fields.Integer(string="Moodle category", required=True)
     moodle_module = fields.Integer()
-    modality_id = fields.Many2many('op.modality')
-    course_id = fields.Many2one('op.course')
+    modality_id = fields.Many2many('op.modality', required=True)
+    course_id = fields.Many2one('op.course', required=True)
