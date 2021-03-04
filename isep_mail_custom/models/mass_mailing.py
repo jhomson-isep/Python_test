dd3038c440cc42c99fb7ddb8a1ef93b8fa09d250
murias <murias@isep.com>
2021-03-04 15:20:53 -0700

    [Fix] mailjet send

--- isep_mail_custom/models/mass_mailing.py
+++ isep_mail_custom/models/mass_mailing.py
@@ -5,9 +5,11 @@ import threading
 import re
 
 from odoo import api, fields, models, tools, _
+from binascii import Error as binascii_error
 from odoo.exceptions import UserError
 from mailjet_rest import Client
 
+_image_dataurl = re.compile(r'(data:image/[a-z]+?);base64,([a-z0-9+/\n]{3,}=*)\n*([\'"])(?: data-filename="([^"]*)")?', re.I)
 _logger = logging.getLogger(__name__)
 
 api_key = '5350a6c8d4d0d5f00422c0204f95c756'
@@ -19,7 +21,8 @@ EMAIL_PATTERN = '([^ ,;<@]+@[^> ,;]+)'
 class MassMailing(models.Model):
     _inherit = 'mail.mass_mailing'
 
-    use_api = fields.Boolean(string='Usar API de Mailjet', required=False, default=True)
+    use_api = fields.Boolean(string='Usar API de Mailjet', required=False,
+                             default=True)
     mailjet_campaign_id = fields.Integer(string='ID de campaña (Mailjet)')
 
     def send_api_mail(self, res_ids=None):
@@ -33,13 +36,18 @@ class MassMailing(models.Model):
 
             while len(res_ids) > 0:
                 recipients = []
+                messages = []
+                recipient = []
                 # _logger.info(mailing.mailing_model_real)
                 extra_context = self._get_mass_mailing_context()
-                recipients_list = self.env[mailing.mailing_model_real].search([('id', 'in', res_ids)], limit=49)
-                recipients_list = recipients_list.with_context(active_ids=res_ids, **extra_context)
+                recipients_list = self.env[mailing.mailing_model_real].search(
+                    [('id', 'in', res_ids)], limit=49)
+                recipients_list = recipients_list.with_context(
+                    active_ids=res_ids, **extra_context)
                 # _logger.info(type(res_ids))
 
-                for rl in recipients_list:
+                for index, rl in enumerate(recipients_list):
+                    recipient = []
                     is_blacklisted = False
                     statistics_values = {
                         'model': mailing.mailing_model_real,
@@ -53,56 +61,73 @@ class MassMailing(models.Model):
                     if mailing.mailing_model_real == "crm.lead":
                         email_to = rl.partner_id.email
                         name = rl.partner_id.name
-                        if not rl.is_blacklisted and not rl.partner_is_blacklisted and re.match(EMAIL_PATTERN,
-                                                                                                email_to):
-                            recipients.append({'Email': email_to, 'Name': name})
-                            statistics_values.update({'email': email_to, 'partner_id': rl.partner_id})
+                        if not rl.is_blacklisted and not rl.partner_is_blacklisted and re.match(
+                                EMAIL_PATTERN, email_to):
+                            recipients.append(
+                                {'Email': email_to, 'Name': name})
+                            recipient.append({'Email': email_to, 'Name': name})
+                            statistics_values.update(
+                                {'email': email_to,
+                                 'partner_id': rl.partner_id})
                         else:
-                            statistics_values.update({'email': email_to, 'partner_id': rl.partner_id,
-                                                      'ignored': fields.Datetime.now(), 'state': 'ignored',
-                                                      'sent': None})
+                            statistics_values.update(
+                                {'email': email_to,
+                                 'partner_id': rl.partner_id,
+                                 'ignored': fields.Datetime.now(),
+                                 'state': 'ignored',
+                                 'sent': None})
                             is_blacklisted = True
                     else:
                         email_to = rl.email
                         name = rl.name
                         if not rl.is_blacklisted and rl.is_email_valid:
-                            recipients.append({'Email': email_to, 'Name': name})
+                            recipients.append(
+                                {'Email': email_to, 'Name': name})
                             statistics_values.update({'email': email_to})
+                            recipient.append({'Email': email_to, 'Name': name})
                         else:
                             statistics_values.update(
-                                {'email': email_to, 'ignored': fields.Datetime.now(), 'state': 'ignored', 'sent': None})
+                                {'email': email_to,
+                                 'ignored': fields.Datetime.now(),
+                                 'state': 'ignored', 'sent': None})
                             is_blacklisted = True
 
-                    # =========== Create mail.mail ===========
+                    # # =========== Create mail.mail ===========
                     mail = self.env['mail.mail']
+                    body = self.env['mail.thread']._replace_local_links(
+                        mailing.body_html)
                     mail_values = {
                         'subject': mailing.name,
                         'email_from': mailing.email_from,
                         'email_to': email_to,
                         'model': mailing.mailing_model_real,
-                        'body': tools.html2plaintext(mailing.body_html),
-                        'body_html': mailing.body_html,
+                        'body': tools.html2plaintext(body),
+                        'body_html': body,
                         'res_id': rl.id,
                         'mailing_id': mailing.id,
                         'state': 'sent',
                         # 'auto_delete': True,
                         'scheduled_date': fields.Datetime.now()
                     }
+                    mail_values = self.prepare_images(mail_values)
                     msg_id = mail.create(mail_values)
                     statistics_values.update(
-                        {'mail_mail_id': msg_id.id, 'mail_mail_id_int': msg_id.id, 'message_id': msg_id.message_id})
+                        {'mail_mail_id': msg_id.id,
+                         'mail_mail_id_int': msg_id.id,
+                         'message_id': msg_id.message_id})
 
                     base_url = self.env[
                         'ir.config_parameter'].sudo().get_param(
                         'web.base.url').rstrip('/')
                     link_to_replace = base_url + '/unsubscribe_from_list'
+                    _logger.info("Link to replace: {}".format(link_to_replace))
                     unsubscribe_url = msg_id._get_unsubscribe_url(email_to)
 
-                    if link_to_replace in mailing.body_html:
-                        msg_id.update({'body_html': mailing.body_html.replace(
+                    if link_to_replace in body:
+                        msg_id.update({'body_html': body.replace(
                             link_to_replace,
                             unsubscribe_url if unsubscribe_url else '#')})
-                        mailing.update({'body_html': mailing.body_html.replace(
+                        mailing.update({'body_html': body.replace(
                             link_to_replace,
                             unsubscribe_url if unsubscribe_url else '#')})
                     # ============= Create mail.mail ============
@@ -110,29 +135,24 @@ class MassMailing(models.Model):
                     # _logger.info("========== is_blacklisted ===========")
                     # _logger.info(is_blacklisted)
                     # _logger.info("========== is_blacklisted ===========")
-                    statistics = self.env['mail.mail.statistics'].with_context(active_ids=res_ids,
-                                                                               **extra_context).create(
+                    statistics = self.env['mail.mail.statistics'].with_context(
+                        active_ids=res_ids, **extra_context).create(
                         statistics_values)
 
-                data = {
-                    'Messages': [
-                        {
+                    message = {
                             "From": {
                                 "Email": mailing.email_from,
                                 "Name": mailing.email_from,
                             },
-                            "To": [
-                                {
-                                    'name': 'Correo ISEP',
-                                    'email': 'certificados1@isep.com'
-                                }
-                            ],
-                            "Bcc": recipients,
+                            "To": recipient,
                             "Subject": mailing.name,
-                            "TextPart": tools.html2plaintext(mailing.body_html),
-                            "HTMLPart": mailing.body_html
+                            "TextPart": tools.html2plaintext(body),
+                            "HTMLPart": body
                         }
-                    ]
+                    messages.append(message)
+
+                data = {
+                    'Messages': messages
                 }
 
                 # _logger.info(data)
@@ -146,23 +166,27 @@ class MassMailing(models.Model):
                     raise UserError(_(
                         "Error on mailjet connection: " % str(result.json())))
                 res_ids = mailing.get_remaining_recipients()
+                messages = []
         return True
 
     @api.model
     def _process_mass_mailing_queue(self):
         mass_mailings = self.search(
-            [('state', 'in', ('in_queue', 'sending')), '|', ('schedule_date', '<', fields.Datetime.now()),
+            [('state', 'in', ('in_queue', 'sending')), '|',
+             ('schedule_date', '<', fields.Datetime.now()),
              ('schedule_date', '=', False)])
         for mass_mailing in mass_mailings:
             user = mass_mailing.write_uid or self.env.user
-            mass_mailing = mass_mailing.with_context(**user.sudo(user=user).context_get())
+            mass_mailing = mass_mailing.with_context(
+                **user.sudo(user=user).context_get())
             # _logger.info(mass_mailing.use_api)
             if not mass_mailing.use_api:
                 if len(mass_mailing.get_remaining_recipients()) > 0:
                     mass_mailing.state = 'sending'
                     mass_mailing.send_mail()
                 else:
-                    mass_mailing.write({'state': 'done', 'sent_date': fields.Datetime.now()})
+                    mass_mailing.write(
+                        {'state': 'done', 'sent_date': fields.Datetime.now()})
             else:
                 # _logger.info("========== Use api ===========")
                 # _logger.info(mass_mailing)
@@ -172,4 +196,42 @@ class MassMailing(models.Model):
                     mass_mailing.write({'state': 'sending'})
                     mass_mailing.send_api_mail()
                 else:
-                    mass_mailing.write({'state': 'done', 'sent_date': fields.Datetime.now()})
+                    mass_mailing.write(
+                        {'state': 'done', 'sent_date': fields.Datetime.now()})
+
+    def prepare_images(self, values):
+        Attachments = self.env['ir.attachment']
+        data_to_url = {}
+        _logger.info('-----------------')
+        _logger.info(values.get('model'))
+
+        def base64_to_boundary(match):
+            key = match.group(2)
+            if not data_to_url.get(key):
+                name = match.group(4) if match.group(
+                    4) else 'image%s' % len(data_to_url)
+                try:
+                    attachment = Attachments.create({
+                        'name': name,
+                        'datas': match.group(2),
+                        'datas_fname': name,
+                        'res_model': values.get('model'),
+                        'res_id': values.get('res_id'),
+                    })
+                except binascii_error:
+                    _logger.warning(
+                        "Impossible to create an attachment out of badly formated base64 embedded image. Image has been removed.")
+                    return match.group(
+                        3)  # group(3) is the url ending single/double quote matched by the regexp
+                else:
+                    attachment.generate_access_token()
+                    values['attachment_ids'].append((4, attachment.id))
+                    data_to_url[key] = ['/web/image/%s?access_token=%s' % (
+                        attachment.id, attachment.access_token), name]
+            return '%s%s alt="%s"' % (
+                data_to_url[key][0], match.group(3), data_to_url[key][1])
+
+        values['body'] = _image_dataurl.sub(base64_to_boundary,
+                                            tools.ustr(values['body']))
+
+        return values
