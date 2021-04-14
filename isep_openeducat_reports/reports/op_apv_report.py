8d6de79fdaaf5601b3a4547023c6ebe5b0d7504b
jhomson-isep <jhomson22@gmail.com>
2021-04-14 11:05:24 -0430

    Reporte-apv-1

---
+++ isep_openeducat_reports/reports/op_apv_report.py
@@ -0,0 +1,8 @@
+from odoo import models
+
+#class OpApvXLS(models.AbstractModel):
+#    _name = 'report.isep_openeducat_reports.op_apv_report'
+#    _inherit = 'report.report_xlsx.abstract'
+
+#    def generate_xlsx_report(self):
+#        print("Exito!!")
